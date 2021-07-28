ed50188b07f07a4698b5bdd31e38353f5a88598b
daskelito <78089256+daskelito@users.noreply.github.com>
2021-07-28 11:44:26 +0200

    Delete EMB_SYS_LAB3 directory

--- EMB_SYS_LAB3/random.h
+++
@@ -1,6 +0,0 @@
-#ifndef RANDOM_H_
-#define RANDOM_H_
-
-int getRandomFreq(int min, int max);
-
-#endif
--- EMB_SYS_LAB3/soundgen.h
+++
@@ -1,9 +0,0 @@
-  
-#ifndef SOUNDGEN_H_
-#define SONDGEN_H_
-
-int startSound(int freq);
-
-void stopSound();
-
-#endif
